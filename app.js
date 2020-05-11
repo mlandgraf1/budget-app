806d1b5e686811a18db6f9955cdcc58fac40650f
mlandgraf1 <42742611+mlandgraf1@users.noreply.github.com>
2020-05-11 17:09:31 -0500

    Define modules

---
+++ app.js
@@ -0,0 +1,61 @@
+//defining the modules
+
+//Budget Controller
+var budgetController = (function() {
+
+
+
+})();
+
+//UI Controller
+var UIController = (function() {
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector('.add__type').value, //will be inc or exp
+                description: document.querySelector('.add_description').value,
+                value: document.querySelector('.add__value').value
+            };
+
+        }
+    }
+
+
+
+})();
+
+//Global App Controller
+var controller = (function(budgetCtrl, UICtrl) {
+
+
+    var ctrlAddItem = function() {
+
+        //1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
+
+        //2. Add the item to the budget controller
+
+        //3. Add the item to the UI
+
+        //4. Calculate the budget
+
+        //5. Display the budget on the UI
+
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+
+
+    })
+
+})(budgetController, UIController);
