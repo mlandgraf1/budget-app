708a3e9a4719601d8f84ad5c2cb4e7c90f4965f1
mlandgraf1 <42742611+mlandgraf1@users.noreply.github.com>
2020-05-11 17:39:53 -0500

    added data structure

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ var budgetController = (function() {
         this.value = value;
     };
 
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+
+    }
+
 })();
 
 //UI Controller
