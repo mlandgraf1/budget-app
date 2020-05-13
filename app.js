2770038deadef730d966e6b7e6446d05d47d7f3a
mlandgraf1 <42742611+mlandgraf1@users.noreply.github.com>
2020-05-12 19:46:13 -0500

    Clear fields after adding item

--- app.js
+++ app.js
@@ -102,6 +102,20 @@ var UIController = (function() {
 
         },
 
+        clearFields: function() {
+            var fields, fieldsArray;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            fieldsArray = Array.prototype.slice.call(fields);
+
+            fieldsArray.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArray[0].focus();
+        },
+
         getDOMstrings: function() {
             return DOMstrings;
         }
@@ -139,9 +153,12 @@ var controller = (function(budgetCtrl, UICtrl) {
         //3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
 
-        //4. Calculate the budget
+        //4. Clear the fields
+        UICtrl.clearFields();
 
-        //5. Display the budget on the UI
+        //5. Calculate the budget
+
+        //6. Display the budget on the UI
 
     }
 
